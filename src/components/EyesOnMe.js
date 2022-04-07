8b0fa4eef1925fb7e2eb464e59f371d38fd6ca9c
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-04-07 17:19:08 -0400

    commit tree ba60a10be8646d424b9619f5ca36490e93508872

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function handleOnBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    function handleOnFocus() {
+        console.log('Good!')
+        }
+        
+        return (
+            <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+            )
+        }
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React from 'react';
+
+function Keypad() {
+    function handleChange(event) {
+        console.log('Entering password...')
+    }
+
+
+    return (
+        <form onChange={handleChange} >
+            <input type="password" name="username" placeholder="Enter username..." />
+            <input type="password" name="password" placeholder="Enter password..." />
+            <button>Login</button>
+        </form>
+    )
+}
+
+export default Keypad
